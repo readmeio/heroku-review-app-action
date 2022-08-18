20fc7bf71c11d58efad3f872187f4ee45ffd1167
Ryan Park <ryanp@readme.io>
2022-08-17 18:17:18 -0700

    more cleanup

--- src/controllers/upsert/index.js
+++ src/controllers/upsert/index.js
@@ -14,10 +14,27 @@ const LogDrainStep = require('./log-drain');
 const PipelineCouplingStep = require('./pipeline-coupling');
 const SetDomainStep = require('./set-domain');
 
-async function upsertController(params) {
-  const sha = git.shaForRef(params.refName); // can't use github.context.sha because we want to exclude merge commits
-  const message = git.messageForRef(params.refName);
+async function getStepsForThisRun(steps) {
+  const queue = [];
+  for (let i = 0; i < steps.length; i += 1) {
+    await steps[i].checkPrereqs(); // eslint-disable-line no-await-in-loop
+    if (steps[i].shouldRun) {
+      queue.push(steps[i]);
+    }
+  }
+  return queue;
+}
 
+async function runSteps(queue) {
+  const stepCount = queue.length + 1;
+  for (let i = 0; i < queue.length; i += 1) {
+    const number = i + 1;
+    core.info(`[Step ${number}/${stepCount}] ${queue[i].title}...`);
+    await queue[i].run(); // eslint-disable-line no-await-in-loop
+  }
+}
+
+async function upsertController(params) {
   const steps = [
     new CreateAppStep(params),
     new PipelineCouplingStep(params),
@@ -30,23 +47,12 @@ async function upsertController(params) {
     new SetDomainStep(params),
   ];
 
-  // Phase 1: check which steps are actually needed for this run, and add them to a queue
-  const queue = [];
-  for (let i = 0; i < steps.length; i += 1) {
-    await steps[i].checkPrereqs(); // eslint-disable-line no-await-in-loop
-    if (steps[i].shouldRun) {
-      queue.push(steps[i]);
-    }
-  }
+  // Check which steps are actually needed for this run, and add them to a
+  // queue; then run everything in the queue.
+  const queue = await getStepsForThisRun(steps);
+  await runSteps(queue);
 
-  // Phase 2: run everything in the queue
-  const stepCount = queue.length + 1;
-  for (let i = 0; i < queue.length; i += 1) {
-    const number = i + 1;
-    core.info(`[Step ${number}/${stepCount}] ${queue[i].title}...`);
-    await queue[i].run(); // eslint-disable-line no-await-in-loop
-  }
-
+  // Print some final output once we're finished
   let appUrl;
   if (params.pipelineName === 'readme') {
     appUrl = `http://${params.appName}.readme.ninja`;
@@ -55,8 +61,11 @@ async function upsertController(params) {
     appUrl = app.web_url;
   }
   core.info(`\nSuccessfully deployed Heroku app "${params.appName}"! Your app is available at:\n    ${appUrl}\n`);
+
+  // ...and post a comment to the pull request
+  const sha = git.shaForRef(params.refName); // can't use github.context.sha because we want to exclude merge commits
+  const message = git.messageForRef(params.refName);
   await comments.postUpsertComment(params.appName, appUrl, sha, message);
-  return true;
 }
 
 module.exports = upsertController;
