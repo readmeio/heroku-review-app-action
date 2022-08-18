26709240142ff4342f9be05ba58e4143372a0405
Ryan Park <ryanp@readme.io>
2022-08-18 12:05:44 -0700

    log docker param

--- src/main.js
+++ src/main.js
@@ -48,6 +48,7 @@ async function getParams() {
 
   params.useDocker = false;
   const dockerParam = core.getInput('docker', { required: false });
+  core.info(`Raw value of docker param = ${dockerParam}`);
   if (dockerParam && dockerParam.length > 0) {
     if (dockerParam === 'true') {
       params.useDocker = true;
